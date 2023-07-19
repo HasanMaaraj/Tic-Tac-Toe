820b289e69ff95d9ed632d8926bd851f1512d932
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-07-19 14:11:53 +0300

    Move rendering the gameboard functionality to the script

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const gameBoard = (function() {
     const player1 = playerFactory('Hasan', 'X');
     const player2 = playerFactory('Foo', 'O');
     let turn = 1;
-    let board = [];
+    let board = ["", "", "", "", "", "", "", "", ""];
     let winner = null;
 
     markPlayer = (box) => {
@@ -18,7 +18,23 @@ const gameBoard = (function() {
         }
     }
 
-    let startGame = function(){
+    const renderBoard = function() {
+        const gameBoard = document.querySelector('.gameboard');
+        // clear the board
+        Array.from(gameBoard.childNodes).forEach((box) => {
+            box.remove()
+        })
+        for (let i=0; i < 9; i++) {
+            const newBox = document.createElement('div');
+            newBox.textContent = board[i];
+            newBox.dataset.index = i+1;
+            newBox.classList = 'box unmarked'
+            gameBoard.appendChild(newBox);
+        }
+    }
+
+    const startGame = function(){
+        renderBoard();
         let boxes = document.querySelectorAll('.box');
         boxes.forEach((box) => {
             // Empty all boxes
